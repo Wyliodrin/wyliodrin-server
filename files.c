4fe8b58292f18a9601d68bf9cf2773eb9a429a67
ioanaculic <ioana_mculic@yahoo.com>
2013-07-19 13:49:23 +0000

    files improved

--- files.c
+++ files.c
@@ -4,17 +4,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "files.h"
 
-struct files *get_files(char *path)
+char **list_files(char *path, int *n)
 {
 	if(path_ok)
     {
         struct dirent **namelist;
-        int n;
 
-        n = scandir(".", &namelist, NULL, alphasort);
+        *n = scandir(".", &namelist, NULL, alphasort);
         if (n < 0)
             {
                 perror("scandir");
@@ -22,41 +22,87 @@ struct files *get_files(char *path)
             }
         else 
         {
-            struct file * f = malloc(sizeof(struct file));
-            f->namelist = namelist;
-            f->n = n;
-           return f;
-        }
-        
+            char **files = malloc((*n) * sizeof(char *));
+            int i;
+            for(i=0; i<*n; i++)
+            {
+                files[i] = namelist[i]->d_name;
+                free(namelist[i]);
+            }
+            free(namelist);
+            return files;
+        }        
     }
     else
         return NULL;	
 }
 
-char ** list_files(char *path)
+file *get_file_info(char *path)
 {
-    struct file * f= get_files(path);
-    if(f == NULL)
-        return NULL;
-    struct dirent **namelist = f->namelist; 
-    int n = f->n;
-    char **files_name = malloc(n * sizeof(char *));
-    int i;
-    for (i=0; i<n; i++)
+    if(path_ok)
     {
-        files_name[i] = namelist[i]->d_name;
-    }
-    return  files_name;
-}
+        struct stat sb;
+        if(stat(path, &sb) < 0)
+        {
+            return NULL;
+        }
+        else
+        {
+            file *f = malloc(sizeof(struct s_file));
+            if(f == NULL)
+                return f;
+            switch (sb.st_mode & S_IFMT) 
+            {
+                case S_IFBLK:  f->type = BLOCK_DEVICE;            break;
+                case S_IFCHR:  f->type = CHARACTER_DEVICE;        break;
+                case S_IFDIR:  f->type = DIRECTORY;               break;
+                case S_IFIFO:  f->type = FIFO;                    break;
+                case S_IFLNK:  f->type = SYMLINK;                 break;
+                case S_IFREG:  f->type = REGULAR_FILE;            break;
+                case S_IFSOCK: f->type = SOCKET;                  break;
+                default:       f->type = UNKNOWN;                 break;
+            }
+            f->atime = sb.st_atime;
+            f->stime = sb.st_ctime;
+            f->mtime = sb.st_mtime;
+            f->size = sb.st_size;
+            int permission = 0;
+            int file_mode = sb.st_mode;
 
-unsigned char get_file_type(char *path)
-{
-    struct file * f= get_files(path);
-    if(f != NULL)
-    {
-       return (f->namelist)[0]->d_type;
+            /*Check owner permissions*/
+             if (file_mode & S_IRUSR)
+                permission = permission + 400;
+            if (file_mode & S_IWUSR) 
+                permission = permission + 200;
+            if (file_mode & S_IXUSR)
+                permission = permission + 100;
+
+            /* Check group permissions */
+            if (file_mode & S_IRGRP)
+                permission = permission + 40;
+            if (file_mode & S_IWGRP)
+                permission = permission + 20;
+            if (file_mode & S_IXGRP)
+                permission = permission + 10;
+
+            /* check other user permissions */
+            if (file_mode & S_IROTH)
+                permission = permission + 4;
+            if (file_mode & S_IWOTH)
+                permission = permission + 2;
+            if (file_mode & S_IXOTH)
+                permission = permission + 1;
+
+            /*check sticky bit*/
+            if(file_mode & S_ISVTX)
+                permission = permission + 1000;
+
+            f->permission = permission;
+
+            return f;
+        }       
     }
-    else return 0;
+    return NULL;
 }
 
 void create_folder(char *path, int permission)
@@ -102,17 +148,23 @@ void write_to_file(char *path, char *buf, int mode)
     }
 }
 
-
 int main()
 {
 	char *pwd = get_current_dir_name();
-    //printf("ls = %c\n",list_files(pwd)[5]);
-   printf("blah = %d\n",get_file_type(pwd));
-    //printf("bla\n");
-    //create_folder("/home/pi/test",0);
-    remove_file("/home/pi/test");
-    create_file("/home/pi/iiii");
-    write_to_file("/home/pi/iiii", "ana", WRITE);
-    write_to_file("/home/pi/iiii", "are", APPEND);
+    int n;
+    //char ** f = list_files(pwd, &n);
+    int i;
+    // for(i=0; i<n; i++)
+    // {
+    //     printf("f=%s\n",f[i]);
+    // }
+    file *f = get_file_info("home/pi/iiii");
+    if(f != NULL)
+    {
+        printf("type = %d\n",f->type);
+        printf("size = %d\n", f->size);
+        printf("modif = %d\n", f->atime);
+        printf("perm = %d\n", f->permission);
+    }
 	return 0;
 }
--- files.h
+++ files.h
@@ -9,14 +9,25 @@
 #define WRITE 	1
 #define APPEND	2
 
+#define BLOCK_DEVICE		1
+#define CHARACTER_DEVICE	2
+#define DIRECTORY			3
+#define FIFO    			4
+#define SYMLINK				8
+#define REGULAR_FILE		5
+#define SOCKET				6
+#define UNKNOWN				7
 
-typedef struct  file
+
+typedef struct  s_file
 {
-	int n;
-	struct dirent **namelist;
-};
-char ** list_files(char *path);
-unsigned char get_file_type(char *path);
+	int type;
+	time_t atime,stime,mtime;
+	long size;
+	int permission;
+}file;
+char **list_files(char *path, int *n);
+file *get_file_info(char *path);
 void remove_file(char *path);
 void create_folder(char *path, int permission);
 void create_file(char *path);
