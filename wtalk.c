4c0d8e9c602c56f2e28de6176b0e4d63e00d6519
Răzvan-Mădălin MATEI <razvan_madalin.matei@cti.pub.ro>
2015-06-25 10:55:58 +0300

    replace fusermount with umount

--- wtalk.c
+++ wtalk.c
@@ -137,7 +137,7 @@ void wtalk() {
 
   /* Unmount the mountFile */
   char umount_cmd[128];
-  rc_int = sprintf(umount_cmd, "fusermount -u %s", mount_file_str);
+  rc_int = sprintf(umount_cmd, "umount %s", mount_file_str);
   wsyserr(rc_int < 0, "sprintf");
   rc_int = system(umount_cmd);
   wsyserr(rc_int == -1, "system");
