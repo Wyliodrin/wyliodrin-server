2058b661e3e44a61fe8aea648e32effea09796bc
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-09-21 16:39:50 +0300

    pretty formatting

--- tests/debugging-redis/run_tests.sh
+++ tests/debugging-redis/run_tests.sh
@@ -3,20 +3,26 @@
 JID_OWNER="wyliodrin_test@wyliodrin.org"
 JID_BOARD="wyliodrin_board@wyliodrin.org"
 PASSWORD="wyliodrin"
+num_tests_passed=0
 
 test_name=(                                                                    \
   "Test start and close debug session"                                         \
   "Test simple run"                                                            \
 )
 
+printf "\n"
+
 for i in `seq 1 ${#test_name[@]}`; do
-  printf "${test_name[$i - 1]} ... "
+  printf "\t[%d/%d]: %-60s - " "$i" "${#test_name[@]}" "${test_name[$i - 1]:0:60}"
 
   /usr/bin/python3 test$i.py -q --jid $JID_OWNER --password $PASSWORD --to $JID_BOARD
 
   if [ $? -eq 0 ]; then
     printf "PASSED\n"
+    num_tests_passed=$(($num_tests_passed + 1))
   else
     printf "FAILED\n"
   fi
 done
+
+printf "\nTotal : %d/%d\n\n" "$num_tests_passed" "${#test_name[@]}"
