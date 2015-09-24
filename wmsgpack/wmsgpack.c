f5e555f431e8f5616d5a899c1d7394fce60c16e6
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-09-24 17:09:53 +0300

    add encoded_msgpack_map_to_hashmap

--- wmsgpack/wmsgpack.c
+++ wmsgpack/wmsgpack.c
@@ -29,8 +29,7 @@
 
 /*** DEFINES *************************************************************************************/
 
-#define MSGPACK_KEY_SIZE     8
-#define MSGPACK_MODULE_SIZE 32
+#define MSGPACK_BUF_SIZE   64
 
 /*************************************************************************************************/
 
@@ -39,6 +38,14 @@
 /*** EXTERN VARIABLES ****************************************************************************/
 
 extern hashmap_p modules; /* from wxmpp_handlers.c */
+
+/*************************************************************************************************/
+
+
+
+/*** STATIC FUNCTIONS DECLARATIONS ***************************************************************/
+
+static hashmap_p encoded_msgpack_map_to_hashmap(const char *encoded_msgpack_map);
 
 /*************************************************************************************************/
 
@@ -55,59 +62,73 @@ void wmsgpack(const char *from, const char *to, int error, xmpp_stanza_t *stanza
     return;
   }
 
-  /* Decode the encoded data */
-  int data_size = strlen(enc_data) * 3 / 4 + 1;
-  char *data = malloc(data_size);
-  if (data == NULL) {
-    werr("malloc: %s", strerror(errno));
+  hashmap_p h = encoded_msgpack_map_to_hashmap(enc_data);
+  char *module = (char *)hashmap_get(h, "m");
+  if (module == NULL) {
+    werr("There is no key named m in msgpack map");
     return;
   }
-  data[data_size - 1] = 0;
-  if (base64_decode((uint8_t *)data, enc_data, data_size) == -1) {
-    werr("Invalid input fed for base64 decoding");
-    return;
+
+  module_fct f = *((module_fct *)hashmap_get(modules, module));
+  f(from, to, error, stanza, conn, userdata);
+}
+
+/*************************************************************************************************/
+
+
+
+/*** STATIC FUNCTIONS IMPLEMENTATIONS ************************************************************/
+
+static hashmap_p encoded_msgpack_map_to_hashmap(const char *encoded_msgpack_map) {
+  /* Decode the encoded data */
+  int msgpack_map_size = strlen(encoded_msgpack_map) * 3 / 4 + 1;
+  char *msgpack_map = calloc(msgpack_map_size, sizeof(char *));
+  if (msgpack_map == NULL) {
+    werr("calloc: %s", strerror(errno));
+    return NULL;
+  }
+  if (base64_decode((uint8_t *)msgpack_map, encoded_msgpack_map, msgpack_map_size) == -1) {
+    werr("Invalid encoded msgpack map");
+    return NULL;
   }
 
   /* Init msgpack */
   cmp_ctx_t cmp;
-  cmp_init(&cmp, data, strlen(data));
+  cmp_init(&cmp, msgpack_map, strlen(msgpack_map));
 
   /* Read msgpack map */
   uint32_t map_size;
   if (!cmp_read_map(&cmp, &map_size)) {
     werr("cmp_read_map: %s", cmp_strerror(&cmp));
-    return;
+    return NULL;
   }
 
-  /* Iterate over map in order to find the module */
   uint32_t i;
-  char key[MSGPACK_KEY_SIZE];
   uint32_t str_size;
+  char key[MSGPACK_BUF_SIZE];
+  char value[MSGPACK_BUF_SIZE];
+  hashmap_p h = create_hashmap();
   for (i = 0; i < map_size; i++) {
-    str_size = MSGPACK_KEY_SIZE;
+    /* Get key */
+    str_size = MSGPACK_BUF_SIZE;
     if (!cmp_read_str(&cmp, key, &str_size)) {
       werr("cmp_read_str: %s", cmp_strerror(&cmp));
-      return;
+      destroy_hashmap(h);
+      return NULL;
     }
 
-    if (strcmp(key, "m") == 0) {
-      /* Read module */
-      char module[MSGPACK_MODULE_SIZE];
-      str_size = MSGPACK_MODULE_SIZE;
-      if (!cmp_read_str(&cmp, module, &str_size)) {
-        werr("cmp_read_str: %s", cmp_strerror(&cmp));
-        return;
-      }
-
-      fprintf(stderr, "Call %s\n", module);
-
-      /* Call module */
-      // module_fct f = *((module_fct *)hashmap_get(modules, module));
-      // f(from, to, error, stanza, conn, userdata);
-
-      return;
+    /* Get value */
+    str_size = MSGPACK_BUF_SIZE;
+    if (!cmp_read_str(&cmp, value, &str_size)) {
+      werr("cmp_read_str: %s", cmp_strerror(&cmp));
+      destroy_hashmap(h);
+      return NULL;
     }
+
+    hashmap_put(h, key, (void *)value, (size_t)(strlen(value)));
   }
+
+  return h;
 }
 
 /*************************************************************************************************/
