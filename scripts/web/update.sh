832ecd5bc440dfce10c423ace137f8dcd80ed728
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-09-29 15:19:20 +0300

    update update.sh

--- scripts/web/update.sh
+++ scripts/web/update.sh
@@ -1,24 +1,17 @@
-#!/bin/sh
-
-###################################################################################################
-# Update libwyliodrin and wyliodrin-server
-###################################################################################################
+#!/bin/bash
 
 
 
-###################################################################################################
-# Script variables
-###################################################################################################
+### Script variables ##############################################################################
 
 SANDBOX_PATH=/sandbox
+HOME=/wyliodrin
 WVERSION=v2.7
 LWVERSION=v1.16
 
 
 
-###################################################################################################
-# Sanity checks
-###################################################################################################
+### Sanity checks #################################################################################
 
 # Test whether the script is run by root or not
 if [ ! "$(whoami)" = "root" ]; then
@@ -42,15 +35,13 @@ fi
 
 
 
-###################################################################################################
-# Actual update
-###################################################################################################
+### Actual update #################################################################################
 
 # Create sandbox
 mkdir -p $SANDBOX_PATH
 
 # Create home
-mkdir -p /wyliodrin
+mkdir -p $HOME
 
 if [ "$wyliodrin_board" = "raspberrypi" ]; then
   CMAKE_PARAMS="-DRASPBERRYPI=ON"
@@ -60,9 +51,7 @@ if [ "$wyliodrin_board" = "raspberrypi" ]; then
   rm -rf wiringPi
   git clone https://github.com/Wyliodrin/wiringPi.git
   cd wiringPi
-  sed 's/sudo//g' build > build2
-  chmod +x build2
-  ./build2
+  ./build
 
   # Install pybass
   cd $SANDBOX_PATH
@@ -82,15 +71,41 @@ if [ "$wyliodrin_board" = "raspberrypi" ]; then
     \"shell_cmd\": \"bash\"\n\
   }\n" > /etc/wyliodrin/settings_raspberrypi.json
 
-printf "\
-[supervisord]\n\
-[program:wtalk]\n\
-command=/usr/bin/wyliodrind\n\
-user=pi\n\
-autostart=true\n\
-autorestart=true\n\
-environment=HOME=\"/wyliodrin\"\n"
-> /etc/supervisor/supervisord.conf
+printf '; supervisor config file
+
+[unix_http_server]
+file=/var/run//supervisor.sock   ; (the path to the socket file)
+chmod=0700                       ; sockef file mode (default 0700)
+
+[supervisord]
+logfile=/var/log/supervisor/supervisord.log ; (main log file;default $CWD/supervisord.log)
+pidfile=/var/run/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
+childlogdir=/var/log/supervisor            ; ('AUTO' child log dir, default $TEMP)
+
+; the below section must remain in the config file for RPC
+; (supervisorctl/web interface) to work, additional interfaces may be
+; added by defining them in separate rpcinterface: sections
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+[supervisorctl]
+serverurl=unix:///var/run//supervisor.sock ; use a unix:// URL  for a unix socket
+
+; The [include] section can just contain the "files" setting.  This
+; setting can list multiple files (separated by whitespace or
+; newlines).  It can also contain wildcards.  The filenames are
+; interpreted as relative to this file.  Included files *cannot*
+; include files themselves.
+
+[include]
+files = /etc/supervisor/conf.d/*.conf
+[supervisord]
+[program:wtalk]
+command=/usr/bin/wyliodrind
+user=pi
+autostart=true
+autorestart=true
+' > /etc/supervisord.conf
 
 elif [ "$wyliodrin_board" = "beagleboneblack" ]; then
   CMAKE_PARAMS="-DBEAGLEBONEBLACK=ON -DNODE_ROOT_DIR=/usr/include"
