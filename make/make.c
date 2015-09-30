ba830edff36d216c52e2f859de5b3753e380eea6
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-09-30 16:50:40 +0300

    archive named after adress

--- make/make.c
+++ make/make.c
@@ -240,10 +240,13 @@ void *fork_thread(void *args) {
   if (pid == 0) {
     char cmd[1024];
     if (!is_fuse_available) {
-      snprintf(cmd, 1023, "cd %s && curl -L -k %s > archive && tar xf archive &&"
-        "rm -rf archive && cd %s/%s && make -f Makefile.%s",
+      snprintf(cmd, 1023, "cd %s && curl %s > %s && tar xf %s &&"
+        "rm -rf %s && cd %s/%s && make -f Makefile.%s",
           build_file_str,
           arg->address_attr,
+          strrchr(arg->address_attr, '/') + 1,
+          strrchr(arg->address_attr, '/') + 1,
+          strrchr(arg->address_attr, '/') + 1,
           build_file_str, projectid_attr,
           board_str);
     } else {
