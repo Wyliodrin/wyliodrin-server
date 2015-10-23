6cb250c21a7d65a8e2a913859fe3c3e5d2b5add4
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-10-23 19:09:14 +0300

    multiple logs in same post

--- tests/localhost/servers/server3.py
+++ tests/localhost/servers/server3.py
@@ -8,30 +8,26 @@ import json
 
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-  post_index = 0
+  l = []
 
   def do_POST(self):
-    if ServerHandler.post_index != 0:
-      length = int(self.headers['Content-Length'])
-      r_json = json.loads(self.rfile.read(length).decode('utf-8'))
+    length = int(self.headers['Content-Length'])
+    r_json = json.loads(self.rfile.read(length).decode('utf-8'))
+    ServerHandler.l += r_json["str"].split("[")[1:]
 
-      if ServerHandler.post_index == 1:
-        if r_json["str"].startswith("[ERROR: ") and "XMPP connection error" in r_json["str"]:
-          print "Second log is ERROR about XMPP connection error"
-        else:
-          print "Second log is not ERROR about XMPP connection error"
-          os._exit(1)
+    if len(ServerHandler.l) >= 3:
+      if ServerHandler.l[1].startswith("ERROR: ") and "XMPP connection error" in ServerHandler.l[1]:
+        print "Second log is ERROR about XMPP connection error"
+      else:
+        print "Second log is not ERROR about XMPP connection error"
+        os._exit(1)
 
-      if ServerHandler.post_index == 2:
-        if r_json["str"].startswith("[ERROR: ") and "Retrying to connect" in r_json["str"]:
-          print "Third log is ERROR about XMPP connection retry"
-          os._exit(0)
-        else:
-          print "Third log is not ERROR about XMPP connection retry"
-          os._exit(1)
-
-    ServerHandler.post_index += 1
-
+      if ServerHandler.l[2].startswith("ERROR: ") and "Retrying to connect" in ServerHandler.l[2]:
+        print "Third log is ERROR about XMPP connection retry"
+        os._exit(0)
+      else:
+        print "Third log is not ERROR about XMPP connection retry"
+        os._exit(1)
 
 httpd = BaseHTTPServer.HTTPServer(('localhost', 443), ServerHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./certificate/server.pem', server_side=True)
