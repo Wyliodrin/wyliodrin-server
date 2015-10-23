af054ed63f0be1b42b9d31ec521eaf54de55457c
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-10-23 13:18:08 +0300

    automated testing

---
+++ tests/localhost/run_tests.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+
+JID="ajid@localhost"
+WYLIODRIN_JSON_PATH="/etc/wyliodrin/wyliodrin.json"
+num_tests_passed=0
+
+
+function replace_wyliodrin_json {
+  # Keep original
+  sudo mv $WYLIODRIN_JSON_PATH $WYLIODRIN_JSON_PATH.orig
+
+  # Write wyliodrin.json
+  printf '{
+  "jid": "%s",
+  "password": "apassword",
+  "owner": "owner@localhost",
+  "ssid": "",
+  "psk": ""
+}' $JID > $WYLIODRIN_JSON_PATH
+}
+
+
+test_name=(                                                                                       \
+  "Test logs are sent to <domain>/gadgets/logs/<jid>"
+)
+
+
+replace_wyliodrin_json
+
+printf "\n\tWyliodrin Automated Testing\n\n"
+
+for i in `seq 1 ${#test_name[@]}`; do
+  printf "[%d/%d]: %-60s - " "$i" "${#test_name[@]}" "${test_name[$i - 1]:0:60}"
+
+  # Start server in background
+  sudo python servers/server$i.py $JID > /dev/null 2>&1 &
+  server_pid=$!
+
+  # Wait for server to start
+  sleep 1
+
+  # Start wyliodrind in background
+  sudo wyliodrind > /dev/null 2>&1 &
+  wyliodrind_pid=$!
+
+  # Wait for server to finish
+  wait $server_pid
+  status=$?
+
+  # Clean
+  sudo kill $wyliodrind_pid
+
+  if [ $status -eq 0 ]; then
+    printf "PASSED\n"
+    num_tests_passed=$(($num_tests_passed + 1))
+  else
+    printf "FAILED\n"
+  fi
+done
+
+printf "\nTotal : %d/%d\n\n" "$num_tests_passed" "${#test_name[@]}"
+
+sudo mv $WYLIODRIN_JSON_PATH.orig $WYLIODRIN_JSON_PATH
---
+++ tests/localhost/servers/server1.py
@@ -0,0 +1,29 @@
+import BaseHTTPServer, SimpleHTTPServer
+import SocketServer
+import ssl
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print "usage: %s <jid>" % (sys.argv[0])
+    os._exit(1)
+
+jid = sys.argv[1]
+
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_POST(self):
+        print "path %s" % (self.path)
+        if self.path == "/gadgets/logs/" + jid:
+            print "Path matches expected path"
+            os._exit(0)
+        else:
+            print "Path does not match expected path"
+            os._exit(1)
+
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 443), ServerHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./certificate/server.pem', server_side=True)
+
+print "Test server up and running"
+httpd.serve_forever()
