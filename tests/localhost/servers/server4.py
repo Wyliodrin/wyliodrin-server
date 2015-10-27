d686243c0ab0ea5d5bedd1788b41dfa9edf08f05
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-10-27 14:43:50 +0200

    Update server4

--- tests/localhost/servers/server4.py
+++ tests/localhost/servers/server4.py
@@ -8,23 +8,20 @@ import json
 
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-  post_index = 0
+  l = []
 
   def do_POST(self):
-    print "POST"
-    if ServerHandler.post_index != 0:
-      length = int(self.headers['Content-Length'])
-      r_json = json.loads(self.rfile.read(length).decode('utf-8'))
+    length = int(self.headers['Content-Length'])
+    r_json = json.loads(self.rfile.read(length).decode('utf-8'))
+    ServerHandler.l += r_json["str"].split("[")[1:]
 
-      if ServerHandler.post_index == 1:
-        if r_json["str"].startswith("[INFO: ") and "XMPP connection established" in r_json["str"]:
-          print "Second log is INFO about XMPP connection success"
-          os._exit(0)
-        else:
-          print "Second log is not INFO about XMPP connection success"
-          os._exit(1)
-
-    ServerHandler.post_index += 1
+    if len(ServerHandler.l) >= 2:
+      if ServerHandler.l[1].startswith("INFO: ") and "XMPP connection established" in ServerHandler.l[1]:
+        print "Second log is INFO about XMPP connection success"
+        os._exit(0)
+      else:
+        print "Second log is not INFO about XMPP connection success"
+        os._exit(1)
 
 
 httpd = BaseHTTPServer.HTTPServer(('localhost', 443), ServerHandler)
