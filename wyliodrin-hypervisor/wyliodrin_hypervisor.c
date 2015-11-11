85c41d584dfeb68d8df4546d32df47662370e93e
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-11-11 16:05:36 +0200

    cleaning

--- wyliodrin-hypervisor/wyliodrin_hypervisor.c
+++ wyliodrin-hypervisor/wyliodrin_hypervisor.c
@@ -38,8 +38,6 @@
 
 /*** STATIC FUNCTIONS DECLARATIONS ***************************************************************/
 
-// static void start_subscriber();
-// static void *start_subscriber_routine(void *arg);
 static void connectCallback(const redisAsyncContext *c, int status);
 static void onMessage(redisAsyncContext *c, void *reply, void *privdata);
 
@@ -48,34 +46,6 @@ static void onMessage(redisAsyncContext *c, void *reply, void *privdata);
 
 
 /*** STATIC FUNCTIONS IMPLEMENTATIONS ************************************************************/
-
-// static void start_subscriber() {
-//   pthread_t t;
-
-//   int pthread_create_rc = pthread_create(&t, NULL, start_subscriber_routine, NULL);
-//   werr2(pthread_create_rc < 0, return, "Could not create thread for start_subscriber_routine");
-
-//   pthread_detach(t);
-// }
-
-
-// static void *start_subscriber_routine(void *arg) {
-//   /* signal(SIGPIPE, SIG_IGN); */
-//   struct event_base *base = event_base_new();
-
-//   redisAsyncContext *c = redisAsyncConnect(REDIS_HOST, REDIS_PORT);
-//   werr2(c == NULL || c->err != 0, return NULL, "redisAsyncConnect error: %s", c->errstr);
-
-//   redisLibeventAttach(c, base);
-//   redisAsyncSetConnectCallback(c, connectCallback);
-//   redisAsyncCommand(c, onMessage, NULL, "SUBSCRIBE %s", REDIS_PUB_CHANNEL);
-//   event_base_dispatch(base);
-
-//   werr("Return from start_subscriber_routine");
-
-//   return NULL;
-// }
-
 
 static void connectCallback(const redisAsyncContext *c, int status) {
   if (status != REDIS_OK) {
@@ -124,25 +94,6 @@ static void onMessage(redisAsyncContext *c, void *reply, void *privdata) {
         shells(r->element[2]->str);
         free(str);
       }
-
-      // werr2(!cmp_read_str(&cmp, str, &str_size),
-      //       return,
-      //       "cmp_read_str error: %s", cmp_strerror(&cmp));
-      // printf("text = %s\n", str);
-
-      // for (i = 0; i < array_size - 2; i += 2) {
-      //   str_size = 64;
-      //   werr2(!cmp_read_str(&cmp, str, &str_size),
-      //         return,
-      //         "cmp_read_str error: %s", cmp_strerror(&cmp));
-      //   printf("attr[%d] key = %s\n", i, str);
-
-      //   str_size = 64;
-      //   werr2(!cmp_read_str(&cmp, str, &str_size),
-      //         return,
-      //         "cmp_read_str error: %s", cmp_strerror(&cmp));
-      //   printf("attr[%d] val = %s\n", i+1, str);
-      // }
     }
   } else {
     werr("Got message on subscription different from REDIS_REPLY_ARRAY");
