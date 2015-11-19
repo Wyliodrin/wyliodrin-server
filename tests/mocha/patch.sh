8a87e16325464296881f18df845a0aac6122a030
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-11-19 15:10:33 +0200

    clean tests

--- tests/mocha/patch.sh
+++ tests/mocha/patch.sh
@@ -2,5 +2,3 @@
 
 cp patch/index.js node_modules/node-xmpp-client/index.js
 cp patch/session.js node_modules/node-xmpp-client/lib/session.js
-cp patch/websockets.js node_modules/node-xmpp-client/lib/websockets.js
-
