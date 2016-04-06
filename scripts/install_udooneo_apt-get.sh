a9af724e9edf131bf0ba3e4e39e70e5a3e894da7
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2016-04-06 18:15:38 +0300

    add udooneo install script via apt-get

---
+++ scripts/install_udooneo_apt-get.sh
@@ -0,0 +1,16 @@
+# Test whether the script is run by root or not
+if [ ! "$(whoami)" = "root" ]; then
+  echo "ERROR: This script must be run as root" 1>&2
+  exit 1
+fi
+
+apt-get update &&
+(cat /etc/apt/sources.list | grep http://wyliodrin.com/public/debian/raspberrypi) || echo "deb http://wyliodrin.com/public/debian/raspberrypi trusty main" >> /etc/apt/sources.list &&
+apt-get update &&
+apt-get install -y --force-yes libstrophe libstrophe-dev &&
+apt-get install -y --force-yes libwyliodrin1 libwyliodrin-dev &&
+apt-get install -y --force-yes wyliodrin-server &&
+apt-get install -y --force-yes wyliodrin-shell &&
+apt-get install -y --force-yes wyliodrin-app-server &&
+apt-get install -y --force-yes node-red &&
+apt-get install -y --force-yes wyliodrin-social
