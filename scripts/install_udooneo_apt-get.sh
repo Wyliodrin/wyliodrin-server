b071f818fc37b7744aac5c0e936bde9a13ac308c
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2016-04-06 18:34:34 +0300

    rename social-libraries

--- scripts/install_udooneo_apt-get.sh
+++ scripts/install_udooneo_apt-get.sh
@@ -8,9 +8,9 @@ apt-get update && \
 (cat /etc/apt/sources.list | grep http://wyliodrin.com/public/debian/udooneo) || echo "deb http://wyliodrin.com/public/debian/udooneo trusty main" >> /etc/apt/sources.list && \
 apt-get update && \
 apt-get install -y --force-yes libstrophe libstrophe-dev && \
-apt-get install -y --force-yes libwyliodrin1 libwyliodrin-dev && \
+apt-get install -y --force-yes libwyliodrin libwyliodrin-dev && \
 apt-get install -y --force-yes wyliodrin-server && \
 apt-get install -y --force-yes wyliodrin-shell && \
 apt-get install -y --force-yes wyliodrin-app-server && \
 apt-get install -y --force-yes node-red && \
-apt-get install -y --force-yes wyliodrin-social
+apt-get install -y --force-yes social-libraries
