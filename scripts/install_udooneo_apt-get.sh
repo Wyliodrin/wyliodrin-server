dfc61bb021ef99a905540271cf20b9a3c523ac60
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2016-04-06 18:24:27 +0300

    singleliner

--- scripts/install_udooneo_apt-get.sh
+++ scripts/install_udooneo_apt-get.sh
@@ -4,13 +4,13 @@ if [ ! "$(whoami)" = "root" ]; then
   exit 1
 fi
 
-apt-get update &&
-(cat /etc/apt/sources.list | grep http://wyliodrin.com/public/debian/udooneo) || echo "deb http://wyliodrin.com/public/debian/udooneo trusty main" >> /etc/apt/sources.list &&
-apt-get update &&
-apt-get install -y --force-yes libstrophe libstrophe-dev &&
-apt-get install -y --force-yes libwyliodrin1 libwyliodrin-dev &&
-apt-get install -y --force-yes wyliodrin-server &&
-apt-get install -y --force-yes wyliodrin-shell &&
-apt-get install -y --force-yes wyliodrin-app-server &&
-apt-get install -y --force-yes node-red &&
+apt-get update && \
+(cat /etc/apt/sources.list | grep http://wyliodrin.com/public/debian/udooneo) || echo "deb http://wyliodrin.com/public/debian/udooneo trusty main" >> /etc/apt/sources.list && \
+apt-get update && \
+apt-get install -y --force-yes libstrophe libstrophe-dev && \
+apt-get install -y --force-yes libwyliodrin1 libwyliodrin-dev && \
+apt-get install -y --force-yes wyliodrin-server && \
+apt-get install -y --force-yes wyliodrin-shell && \
+apt-get install -y --force-yes wyliodrin-app-server && \
+apt-get install -y --force-yes node-red && \
 apt-get install -y --force-yes wyliodrin-social
